72df2fab98e84fec3c5ebfb135a9c28b46adcbe0
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-08-04 19:45:14 +0100

    Include sass and clean-css into workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,44 @@
 'use strict';
 
-// get dependenceis
+// ------ GET DEPENDENCIES ------- //
 var gulp = require('gulp'),
 	pug = require('gulp-pug'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	sass = require('gulp-sass'),
+	cleanCSS = require('gulp-clean-css');
 
-// define task for pug
+// ------ HTML PRE-PROCESSOR: 'PUG' ------- //
 gulp.task('pug', function(){
-	// get files from src
 	return gulp.src('src/*.pug')
-	// run pug on files, output to dist
 		.pipe(pug())
 		.pipe(gulp.dest('dist'));
 });
 
+// ------ CSS PRE-PROCESSOR: 'SASS' ------- //
+gulp.task('sass', function(){
+  	return gulp.src('src/scss/*.scss')
+    	.pipe(sass())
+    	.pipe(gulp.dest('src/css'))
+    	.pipe(browserSync.reload({ // do I need to run this again here?
+      		stream: true
+    	}))
+});
+
+// ------ CSS CLEAN UP AND COMPRESS ------- //
+gulp.task('minify-css', function(){
+	return gulp.src('src/css/*.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('dist/css'));
+});
+
  
-gulp.task('pug-rebuild', ['pug'], function(){
+gulp.task('pug-rebuild', ['pug'], function(){ // do I need to pass something else here to get reload?
 	browserSync.reload();
 });
 
 
-gulp.task('browser-sync', ['pug'], function () {
+// ------ BROWSER SYNC... runs a local server ------- //
+gulp.task('browser-sync', ['pug'], function () { // does 'sass' belong here in the array?
 	browserSync({
 		server: {
 			baseDir: 'dist'
@@ -29,10 +47,13 @@ gulp.task('browser-sync', ['pug'], function () {
 	});
 });
 
+// ------ GULP WATCH FOR FILE CHANGES ------- //
 gulp.task('watch', function () {
-	// watching for changes in src, if yes will run pug-rebuild
+	// watching for changes in these locations... if yes gulp will run these tasks
 	gulp.watch(['src/*.pug'], ['pug-rebuild']);
+	gulp.watch(['src/scss/*.scss'], ['sass']); // new
+	gulp.watch(['src/css/*.css'], ['minify-css']);
 });
 
-// default command to run tasks
+// ------ TASKS TO RUN WHEN USING THE COMMAND 'gulp' IN THE TERMINAL/COMMAND LINE ------- //
 gulp.task('default', ['browser-sync', 'watch']);
