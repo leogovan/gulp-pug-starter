564bd99d6cdbf874c96777ec9c62954a3f521c1f
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-08-04 23:17:41 +0100

    Updated to get browsersync to work

--- gulpfile.js
+++ gulpfile.js
@@ -3,56 +3,67 @@
 // ------ GET DEPENDENCIES ------- //
 var gulp = require('gulp'),
 	pug = require('gulp-pug'),
-	browserSync = require('browser-sync'),
+	browserSync = require('browser-sync').create(),
 	sass = require('gulp-sass'),
-	cleanCSS = require('gulp-clean-css');
+	cleanCSS = require('gulp-clean-css'),
+	uglify = require('gulp-uglify');
 
 // ------ HTML PRE-PROCESSOR: 'PUG' ------- //
-gulp.task('pug', function(){
+gulp.task('pug', function () {
 	return gulp.src('src/*.pug')
 		.pipe(pug())
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({
+    		stream: true
+    	}));
 });
 
 // ------ CSS PRE-PROCESSOR: 'SASS' ------- //
-gulp.task('sass', function(){
+gulp.task('sass', function () {
   	return gulp.src('src/scss/*.scss')
     	.pipe(sass())
     	.pipe(gulp.dest('src/css'))
-    	.pipe(browserSync.reload({ // do I need to run this again here?
-      		stream: true
-    	}))
+    	.pipe(browserSync.reload({
+    		stream: true
+    	}));
 });
 
 // ------ CSS CLEAN UP AND COMPRESS ------- //
-gulp.task('minify-css', function(){
+gulp.task('minify-css', function () {
 	return gulp.src('src/css/*.css')
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('dist/css'));
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.reload({
+    		stream: true
+    	}));
 });
 
- 
-gulp.task('pug-rebuild', ['pug'], function(){ // do I need to pass something else here to get reload?
-	browserSync.reload();
+// ------ JS CLEAN UP AND COMPRESS ------- //
+gulp.task('compress-js', function () {
+	return gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'))
+		.pipe(browserSync.reload({
+    		stream: true
+    	}));
 });
-
 
 // ------ BROWSER SYNC... runs a local server ------- //
-gulp.task('browser-sync', ['pug'], function () { // does 'sass' belong here in the array?
-	browserSync({
+gulp.task('browser-sync', function () {
+	browserSync.init({
 		server: {
 			baseDir: 'dist'
 		},
-		notify: false
-	});
+	})
 });
 
 // ------ GULP WATCH FOR FILE CHANGES ------- //
-gulp.task('watch', function () {
+gulp.task('watch', ['browser-sync', 'sass', 'pug', 'minify-css', 'compress-js'], function () {
 	// watching for changes in these locations... if yes gulp will run these tasks
-	gulp.watch(['src/*.pug'], ['pug-rebuild']);
-	gulp.watch(['src/scss/*.scss'], ['sass']); // new
+	gulp.watch(['src/*.pug'], ['pug']);
+	gulp.watch(['src/scss/*.scss'], ['sass']);
 	gulp.watch(['src/css/*.css'], ['minify-css']);
+	gulp.watch(['src/js/*.js'], ['compress-js']);
 });
 
 // ------ TASKS TO RUN WHEN USING THE COMMAND 'gulp' IN THE TERMINAL/COMMAND LINE ------- //
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var deleteMePlease = function(){
+	alert("Great! If you've seen me, the JS build is working.");
+};
+
+deleteMePlease();
